659cbe54f9fca9371a9a7786f2857ea211dd4acc
mstresing <mstresing@gmail.com>
2024-09-02 22:22:01 +0100

    WIP: uber-basic landing page w formsubmit (to be configured for new email)

---
+++ lagels-landing/src/App.js
@@ -0,0 +1,28 @@
+import { ReactComponent as NetworkImg } from './network.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+     <h1 className='company-logo'>Lagels</h1>
+     <div className='center-content'>
+      <h2 className='pitch-line'>Never leave your clients waiting for simple answers again.</h2>
+      <p className='description'>
+        Intuitive. Individualised. 
+        Gather the information you need, manage and deliver accurate responses to queries, have more time to focus delivering the best legal service possible.
+      </p>
+      <form target="_blank" action="https://formsubmit.co/fcf823ec808ee68a0afc6de035f2a387" method="POST">
+        <div class="form-group">
+          <input type="email" name="email" className="email-input" placeholder="name@firm.com" required/>
+          <button type="submit" className='demo-request'>Request demo</button>
+        </div>
+      </form>
+          
+      <NetworkImg className='network-img'/>
+     </div>
+    </div>
+  );
+}
+
+export default App;
+
