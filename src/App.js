4490192c71263f0215ace8cb3739abd0c2003825
mstresing <mstresing@gmail.com>
2024-09-05 17:56:29 +0100

    Update web3form key

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import { ReactComponent as NetworkImg } from './network.svg';
-import './App.css';
-import logo from './logo.png';
+import { ReactComponent as NetworkImg } from './network.svg'
+import logo from './logo.png'
+import './App.css'
 
 function App() {
   return (
@@ -15,18 +15,16 @@ function App() {
           Revolutionising legal workflows with a transparent, AI-powered platform built on the world’s first open-source legal process database. Automate tasks, customise processes, and access trusted, data-driven insights to streamline legal work for both customers and professionals—anytime, anywhere.
         </p>
 
-        {/* Custom email form starts here */}
         <form className="contact-form" action="https://api.web3forms.com/submit" method="POST">
-          <input type="hidden" name="access_key" value="cdc19d27-6de8-4eca-8b1c-671a2971893c" />
-          <input type="hidden" name="subject" value="New Contact Form Submission" />
-          <input type="hidden" name="from_name" value="My Website" />
+          <input type="hidden" name="access_key" value="24f7b82a-1e0f-40e4-ae42-eac14de53d14" />
+          <input type="hidden" name="subject" value="Demo Request" />
+          {/* <input type="hidden" name="from_name" value="My Website" /> */}
 
           <div className="form-group">
             <input className="email-input" type="email" name="email" placeholder="Your email" required />
             <button className="demo-request" type="submit">Request Demo</button>
           </div>
         </form>
-        {/* Custom email form ends here */}
 
         <NetworkImg className='network-img' />
       </div>
